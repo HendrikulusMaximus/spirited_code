c68f52835aae7379f614a1e476299052135093ec
TheMarkus <hendrik.markus020406@gmail.com>
2024-09-22 14:28:31 +0200

    Added Scanner for input and parser into Stringarray.

--- src/2023/day-21/Java/TheMarkus/AoC21/src/Main.java
+++ src/2023/day-21/Java/TheMarkus/AoC21/src/Main.java
@@ -1,15 +1,43 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        String[][] map = scanThatMap();
+
+    }
+
+    private static void loadLineIntoMap(String line, String[][] map, int currentY){
+        char[] chars = line.toCharArray();
+        for (int i = 0; i < line.length(); i++){
+            map[i][currentY] = String.valueOf(chars[i]);
         }
     }
+
+    private static String[][] scanThatMap() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter that map: ");
+        String input = "";
+        String[][] map = new String[0][0];
+        int currentLine = 0;
+
+        if (sc.hasNext()) {
+            input=sc.next();
+            map = new String[input.length()][input.length()];
+            loadLineIntoMap(input, map, currentLine++);
+
+            while (sc.hasNext()){
+                input=sc.next();
+                loadLineIntoMap(input, map, currentLine++);
+            }
+        }
+        return map;
+    }
+
+
 }
